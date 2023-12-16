4f10aa16817bb8ca1f076a0c0c526942622ccdce
rlanggithub <r0556721183@gmail.com>
2023-12-16 19:35:09 +0200

    nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+// import './App.css';
 import './App.css';
-
+import  React ,{Suspense}from 'react';
+import { Route,Link,Routes} from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import { cyan, teal,lime } from '@mui/material/colors';
+import Tab from '@mui/material/Tab';
+const LazyHome=React.lazy(()=>import('./componets/home/home'))
+const LazyToDo=React.lazy(()=>import('./componets/toDo/toDo'))
+const LazyPost=React.lazy(()=>import('./componets/post/post'))
+const LazyPoto=React.lazy(()=>import('./componets/photo/photo'))
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Box sx={{ display: { xs: 'none', sm: 'block' }, '& :not(style)': { ml: 2} }}>
+        <AppBar position="static" sx={{ backgroundColor: teal["500"]}} >
+          <Toolbar >
+            <Link to="/" underline='none'>
+               <Tab label="בית" sx={{color:lime['50'],fontSize:'20px'}}  />
+            </Link>
+            <Link to="/todo" underline="none" >
+             <Tab label="משימות" sx={{color:lime['50'],fontSize:'20px'}}  />
+            </Link>
+            <Link to="/post" underline="none">
+            <Tab label="פוסט" sx={{color:lime['50'],fontSize:'20px'}}  /> 
+            </Link>
+            <Link to="/photo" underline="none">
+             <Tab label="תמונות" sx={{color:lime['50'],fontSize:'20px'}}  /> 
+            </Link>
+            <Link to="/users" underline="hover">
+             <Tab label="משתמשים" sx={{color:lime['50'],fontSize:'20px'}}  /> 
+            </Link>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Routes>
+        {/* מותאם למעלה */}
+        <Route path="/"  element={<Suspense fallback={<h1>loading..</h1>}><LazyHome/></Suspense>} />
+        <Route path="/toDo"  element={<Suspense fallback={<h1>loading..</h1>}><LazyToDo/></Suspense>} />
+        <Route path="/post"  element={<Suspense fallback={<h1>loading..</h1>}><LazyPost/></Suspense>} />
+        <Route path="/photo"  element={<Suspense fallback={<h1>loading..</h1>}><LazyPoto/></Suspense>} />
+      </Routes>
+    </>
+      
   );
 }
 
